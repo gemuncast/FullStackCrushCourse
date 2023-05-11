07e402649aca8fd1fc53fead994051d9a3c4cdbb
gemuncast <gemuncast@gmail.com>
2023-05-11 12:30:39 -0400

    fix leak

--- V1/script.js
+++ V1/script.js
@@ -24,10 +24,8 @@ async function loadFacts() {
     "https://sbpdocfncpsfkviqodey.supabase.co/rest/v1/facts",
     {
       headers: {
-        apikey:
-          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNicGRvY2ZuY3BzZmt2aXFvZGV5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODM1Nzg5MjMsImV4cCI6MTk5OTE1NDkyM30.-48aF_iKfS1cvErUfyOtv-QQZmDWUr0LRa2ZJRCB_Sw",
-        authorization:
-          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNicGRvY2ZuY3BzZmt2aXFvZGV5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODM1Nzg5MjMsImV4cCI6MTk5OTE1NDkyM30.-48aF_iKfS1cvErUfyOtv-QQZmDWUr0LRa2ZJRCB_Sw",
+        apikey: "test",
+        authorization: "Bearer test",
       },
     }
   );
